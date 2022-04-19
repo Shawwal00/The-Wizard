cb5c9f1edc59e228e2c41ad2b87a7c453d479b8e
zenna <zennamaltby2003@gmail.com>
2022-04-19 11:22:59 +0100

    End screen working
    When you press yes it takes you to the story screen. Need to get it to reset all points/health when we get to that

---
+++ Assets/EndScreen.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    public string firstlevel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+}
