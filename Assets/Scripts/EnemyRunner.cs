9f05dab876bf1fc0b160c10fa75d8cc80b8aa2d8
Shawwal <Shawwal2.Shahid@live.uwe.ac.uk>
2022-04-20 16:37:52 +0100

    Began working on the enemy

---
+++ Assets/Scripts/EnemyRunner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRunner : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+}
