5225e754136df10d3ef318098473418d53ba9df4
Shawwal <Shawwal2.Shahid@live.uwe.ac.uk>
2022-04-21 01:22:58 +0100

    Enemy done

--- Assets/Scripts/EnemyRunner.cs
+++ Assets/Scripts/EnemyRunner.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class EnemyRunner : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 7f;
+    private SpriteRenderer sprite;
+    [SerializeField] private float moveSpeed = 0.01f;
+    private float direction = -1;
+    public GameObject boundsLeft;
+    public GameObject boundsRight;
+    public GameObject StartingPos;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-     
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(new Vector2( direction * 1, 0) * Time.deltaTime * moveSpeed);
+        if (gameObject.transform.position.x <= boundsLeft.transform.position.x)
+        {
+            direction = direction * -1;
+            sprite.flipX = true;
+        }
 
+        else if (gameObject.transform.position.x >= boundsRight.transform.position.x)
+        {
+            direction = direction * -1;
+            sprite.flipX = false;
+        }
     }
 
-    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject PlayerCharachter = GameObject.FindGameObjectWithTag("Player");
+        Collider2D PlayerCollider = PlayerCharachter.GetComponent<Collider2D>();
+
+        if (other == PlayerCollider)
+        {
+            PlayerCharachter.transform.position = StartingPos.transform.position;
+        }
+    }
+
 }
